d05380384d1d67b60eab6237737e05919c47684a
Jacob Johnson <jmjohnson1578@Gmail.com>
2024-07-26 13:58:54 -0600

    setup-scheduler step complete

---
+++ tethysapp-dask_tutorial/tethysapp/dask_tutorial/app.py
@@ -0,0 +1,30 @@
+from tethys_sdk.base import TethysAppBase
+from tethys_sdk.app_settings import SchedulerSetting
+
+
+class App(TethysAppBase):
+    """
+    Tethys app class for Dask Tutorial.
+    """
+    name = 'Dask Tutorial'
+    description = ''
+    package = 'dask_tutorial'  # WARNING: Do not change this value
+    index = 'home'
+    icon = f'{package}/images/icon.gif'
+    root_url = 'dask-tutorial'
+    color = '#27ae60'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def scheduler_settings(self):
+        scheduler_settings = (
+            SchedulerSetting(
+                name='dask_primary',
+                description='Scheduler for a Dask distributed cluster.',
+                engine=SchedulerSetting.DASK,
+                required=True
+            ),
+        )
+
+        return scheduler_settings
